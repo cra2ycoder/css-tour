a63db84e009a540a4f5cbf16436b92d9a7a5f8fb
cra2ycoder <49195865+cra2ycoder@users.noreply.github.com>
2021-05-17 23:58:29 +0530

    css alignment fixes

--- components/CSS/CSSOptions.tsx
+++ components/CSS/CSSOptions.tsx
@@ -1,6 +1,6 @@
 import { memo, useEffect } from "react";
 import { useFormik } from "formik";
-import { Checkbox, FormControlLabel, Typography } from "@material-ui/core";
+import { Box, Checkbox, FormControlLabel, Typography } from "@material-ui/core";
 
 function CSSOptions(props: any) {
   const { options = {}, onChange = () => {} } = props;
@@ -13,31 +13,32 @@ function CSSOptions(props: any) {
   });
 
   useEffect(() => {
-    // console.log(formik.values);
-    onChange(formik.values);
-  }, [formik.values]);
+    onChange(formik?.values);
+  }, [formik?.values]);
 
   return (
-    <>
+    <Box flexWrap="wrap">
       <Typography variant="overline">CSS Properties:</Typography>
-      {Object.keys(options).map((key: string, idx: number) => {
-        return (
-          <FormControlLabel
-            key={`form-control-${idx}`}
-            label={key}
-            control={
-              <Checkbox
-                key={`button-${idx}`}
-                id={key}
-                name={key}
-                onChange={formik.handleChange}
-                title={key}
-              />
-            }
-          />
-        );
-      })}
-    </>
+      <Box>
+        {Object.keys(options).map((key: string, idx: number) => {
+          return (
+            <FormControlLabel
+              key={`form-control-${idx}`}
+              label={key}
+              control={
+                <Checkbox
+                  key={`button-${idx}`}
+                  id={key}
+                  name={key}
+                  onChange={formik.handleChange}
+                  title={key}
+                />
+              }
+            />
+          );
+        })}
+      </Box>
+    </Box>
   );
 }
 
